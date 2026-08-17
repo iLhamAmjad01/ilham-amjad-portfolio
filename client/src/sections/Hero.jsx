8df72f2b973d6eb7fee7f9cbcd345eab756d9e03
iLham Amjad <ilhamamjad4050@gmail.com>
2026-08-17 23:43:03 +0500

    minor changes

--- client/src/sections/Hero.jsx
+++ client/src/sections/Hero.jsx
@@ -206,7 +206,7 @@ const Hero = () => {
               style={{ color: '#22D3EE' }}
             >
               <span className="opacity-70 mr-1">{'>'}</span>
-              Full Stack Developer
+              Full Stack Developer ,React developer , Web Designer , Seo Specalist, AI Automation Engineer,Ios Developer, App Developer
               <Cursor />
             </motion.p>
 
